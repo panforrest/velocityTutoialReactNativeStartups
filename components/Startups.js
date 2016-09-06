70ab9880c3d0280b86faba0698bdd80fdf8feff7
panforrest <guoqianp@gmail.com>
2016-09-05 23:09:27 -0400

    ListView

--- components/Startups.js
+++ components/Startups.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
 import {
   Text,
-  View
+  View,
+  ListView
 } from 'react-native'
 
 class Startups extends Component {
 
+    constructor(props, context){
+    	super(props, context)
+    	const startups = [
+            {id:0, name:'uber', industry:'transportation'},
+            {id:1, name:'air bnb', industry:'hospitality'},
+            {id:2, name:'instagram', industry:'social'}
+    	]
+
+    	const dataSource = new ListView.DataSource({
+    		rowHasChanged: (r1, r2) => r1 !== r2
+    	})
+
+    	this.state = {
+    		startups: dataSource.cloneWithRows(startups)
+    	}
+    }
+
+    renderRow(startup, sId, rId){
+    	return (
+    		<Text>{startup.name}</Text>
+    	)
+    }	
+
 	render(){
 		return (
-
             <View>
-	            <Text>
-	                This is the Startups Component
-	            </Text>
+                <ListView
+                    dataSource={this.state.startups}
+                    renderRow={this.renderRow} />
 	        </View>
 	    )
 	}
